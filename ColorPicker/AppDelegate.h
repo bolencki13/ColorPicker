5545e0c5c564ecbeb5354a5ba7a10a94433fc51f
Brian Olencki <bolencki13@Brians-MacBook-Pro-3.local>
2015-04-28 18:31:45 -0400

    Initial Commit

---
+++ ColorPicker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ColorPicker
+//
+//  Created by Brian Olencki on 4/28/15.
+//  Copyright (c) 2015 bolencki13. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ColorPicker/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ColorPicker
+//
+//  Created by Brian Olencki on 4/28/15.
+//  Copyright (c) 2015 bolencki13. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
